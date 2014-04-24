9898195472cfa7f21e23557f6a5bcb0bebea1ffb
Durand Fabrice <fdurand@inverse.ca>
2014-04-24 13:28:35 -0400

    Frontend

--- datafinger/views.py
+++ datafinger/views.py
@@ -4,10 +4,13 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect, HttpRequest
 from django.views.generic import ListView
 from django.forms.models import modelformset_factory
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.core.context_processors import csrf
+from django.contrib.auth import authenticate, login, logout
 
 def logon(request):
     if request.user.is_authenticated():
-        return HttpResponseRedirect("/finger/")
+        return HttpResponseRedirect("/fingerprints/")
     if request.POST:
         c = {}
         c.update(csrf(request))
@@ -22,7 +25,9 @@ def logon(request):
                 except:
                     ipsource = request.META['REMOTE_ADDR']
                     login(request, user)
-                return HttpResponseRedirect("/finger/")
+                return HttpResponseRedirect("/fingerprints/")
+        else:
+            return render_to_response('logon.html',context_instance=RequestContext(request))
     else:
         return render_to_response('logon.html',context_instance=RequestContext(request))
  
@@ -33,17 +38,110 @@ def disconnect(request):
     return HttpResponseRedirect("/logon/")
 
 class dhcp(ListView):
-    template_name = 'home.html'
+    template_name = 'fingerprint.html'
     queryset = FINGERPRINT.objects.order_by('dhcp_fingerprint')
     context_object_name = 'dhcp'
 
-def test(request):
-    #DHCPFormu = DHCPForm()
-    FINGERPRINTForm = modelformset_factory(FINGERPRINT)
-    if request.method == 'POST':
-        formset = FINGERPRINTForm(request.POST, request.FILES)
-        if formset.is_valid():
-            formset.save()
-    else:
-        formset = FINGERPRINTForm()
-    return render_to_response('dhcp.html', {"formset": formset})
+class device(ListView):
+    template_name = 'device.html'
+    queryset = Device.objects.order_by('device_desc')
+    context_object_name = 'device'
+
+class devicefamily(ListView):
+    template_name = 'devicefamily.html'
+    queryset = Device_Family.objects.order_by('device_family')
+    context_object_name = 'Device_Family'
+
+
+class osfamily(ListView):
+    template_name = 'osfamily.html'
+    queryset = OS_Family.objects.order_by('os_family')
+    context_object_name = 'os_family'
+
+
+class ostype(ListView):
+    template_name = 'ostype.html'
+    queryset = OS_Type.objects.order_by('os_type')
+    context_object_name = 'os_type'
+
+class os(ListView):
+    template_name = 'os.html'
+    queryset = OS.objects.order_by('os')
+    context_object_name = 'os'
+
+class mac(ListView):
+    template_name = 'mac.html'
+    queryset = MAC.objects.order_by('oui')
+    context_object_name = 'mac'
+
+def fingerprint_edit(request,nid):
+    fingerprint = FINGERPRINT.objects.get(id=nid)
+    form = FINGERPRINTForm(instance=fingerprint)
+    return render_to_response('fingerprint.html', locals(),context_instance=RequestContext(request))
+
+def fingerprint_delete(request,nid):
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect("/logon/")
+    fingerprint = FINGERPRINT.objects.get(id=nid)
+    fingerprint.delete()
+    return HttpResponseRedirect("/fingerprints/")
+
+def device_edit(request,nid):
+    device = Device.objects.get(id=nid)
+    form = DeviceForm(instance=device)
+    return render_to_response('device.html', locals(),context_instance=RequestContext(request))
+
+def device_delete(request,nid):
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect("/logon/")
+    device = Device.objects.get(id=nid)
+    device.delete()
+    return HttpResponseRedirect("/devices/")
+
+def devicefamily_edit(request,nid):
+    device = Device_Family.objects.get(id=nid)
+    form = Device_FamilyForm(instance=device)
+    return render_to_response('device.html', locals(),context_instance=RequestContext(request))
+
+def devicefamily_delete(request,nid):
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect("/logon/")
+    device = Device_Family.objects.get(id=nid)
+    device.delete()
+    return HttpResponseRedirect("/devices/")
+
+def os_edit(request,nid):
+    os = OS.objects.get(id=nid)
+    form = OSForm(instance=os)
+    return render_to_response('os.html', locals(),context_instance=RequestContext(request))
+
+def os_delete(request,nid):
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect("/logon/")
+    os = OS.objects.get(id=nid)
+    os.delete()
+    return HttpResponseRedirect("/os/")
+
+def osfamily_edit(request,nid):
+    os = OS_Family.objects.get(id=nid)
+    form = OS_FamilyForm(instance=os)
+    return render_to_response('os.html', locals(),context_instance=RequestContext(request))
+
+def osfamily_delete(request,nid):
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect("/logon/")
+    os = OS_Family.objects.get(id=nid)
+    os.delete()
+    return HttpResponseRedirect("/os/")
+
+def ostype_edit(request,nid):
+    os = OS_Type.objects.get(id=nid)
+    form = OS_TypeForm(instance=os)
+    return render_to_response('os.html', locals(),context_instance=RequestContext(request))
+
+def ostype_delete(request,nid):
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect("/logon/")
+    os = OS_Type.objects.get(id=nid)
+    os.delete()
+    return HttpResponseRedirect("/os/")
--- fingerprint/urls.py
+++ fingerprint/urls.py
@@ -2,13 +2,32 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from datafinger.views import dhcp
+from datafinger.views import dhcp, device, os, osfamily, ostype, devicefamily, mac
 
 urlpatterns = patterns('',
     url(r'^/?$', 'datafinger.views.logon'),
     url(r'^logon/$', 'datafinger.views.logon'),
     url(r'^logout/$', 'datafinger.views.disconnect'),
-    url(r'^finger/$', dhcp.as_view()),
-    url(r'^test/$', 'datafinger.views.test'),
+    url(r'^fingerprints/$', dhcp.as_view()),
+    url(r'^fingerprints/edit/(?P<nid>\d+)/$', 'datafinger.views.fingerprint_edit'),
+    url(r'^fingerprints/del/(?P<nid>\d+)/$', 'datafinger.views.fingerprint_delete'),
+    url(r'^devices/$', device.as_view()),
+    url(r'^devices/edit/(?P<nid>\d+)/$', 'datafinger.views.device_edit'),
+    url(r'^devices/del/(?P<nid>\d+)/$', 'datafinger.views.device_delete'),
+    url(r'^devicesfamily/$', devicefamily.as_view()),
+    url(r'^devicesfamily/edit/(?P<nid>\d+)/$', 'datafinger.views.devicefamily_edit'),
+    url(r'^devicesfamily/del/(?P<nid>\d+)/$', 'datafinger.views.devicefamily_delete'),
+    url(r'^osfamily/$', osfamily.as_view()),
+    url(r'^osfamily/edit/(?P<nid>\d+)/$', 'datafinger.views.osfamily_edit'),
+    url(r'^osfamily/del/(?P<nid>\d+)/$', 'datafinger.views.osfamily_delete'),
+    url(r'^ostype/$', ostype.as_view()),
+    url(r'^ostype/edit/(?P<nid>\d+)/$', 'datafinger.views.ostype_edit'),
+    url(r'^ostype/del/(?P<nid>\d+)/$', 'datafinger.views.ostype_delete'),
+    url(r'^os/$', os.as_view()),
+    url(r'^os/edit/(?P<nid>\d+)/$', 'datafinger.views.os_edit'),
+    url(r'^os/del/(?P<nid>\d+)/$', 'datafinger.views.os_delete'),
+    url(r'^mac/$', mac.as_view()),
+    url(r'^mac/edit/(?P<nid>\d+)/$', 'datafinger.views.mac_edit'),
+    url(r'^mac/del/(?P<nid>\d+)/$', 'datafinger.views.mac_delete'),
     url(r'^admin/', include(admin.site.urls)),
 )
