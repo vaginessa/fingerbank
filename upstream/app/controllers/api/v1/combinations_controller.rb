64deec327db65c496aba2b971e38be0ed1c4f493
Julien Semaan <jul.semaan@gmail.com>
2014-11-14 15:40:12 -0500

    made interogate use the new discoverer interface instead of old algo

--- upstream/app/controllers/api/v1/combinations_controller.rb
+++ upstream/app/controllers/api/v1/combinations_controller.rb
@@ -20,57 +20,27 @@ class Api::V1::CombinationsController < Api::ApiController
     @combination = nil
     user_agent = UserAgent.where(:value => interogate_params[:user_agent]).first
     user_agent = UserAgent.create(:value => interogate_params[:user_agent]) unless user_agent
+    puts "Matched UA #{user_agent.id} : #{user_agent.value}"
 
     dhcp_fingerprint = DhcpFingerprint.where(:value => interogate_params[:dhcp_fingerprint]).first
     dhcp_fingerprint = DhcpFingerprint.create(:value => interogate_params[:dhcp_fingerprint]) unless dhcp_fingerprint
+    puts "Matched DHCP fingerprint #{dhcp_fingerprint.id} : #{dhcp_fingerprint.value}"
 
     dhcp_vendor = DhcpVendor.where(:value => interogate_params[:dhcp_vendor]).first
     dhcp_vendor = DhcpVendor.create(:value => interogate_params[:dhcp_vendor]) unless dhcp_vendor
+    puts "Matched DHCP vendor #{dhcp_vendor.id} : #{dhcp_vendor.value}"
 
     mac_vendor = MacVendor.from_mac(interogate_params[:mac])
     mac_vendor_id = mac_vendor.nil? ? 'NULL' : mac_vendor.id
 
-    matched = Combination.where("user_agent_id=#{user_agent.id} or dhcp_fingerprint_id=#{dhcp_fingerprint.id} or dhcp_vendor_id=#{dhcp_vendor.id} or mac_vendor_id=#{mac_vendor_id}")
-
-    puts matched
-
-    unless matched.empty?
-      top = nil
-      top_matched = 0
-      matched = matched.sort{|a,b| a.score <=> b.score}
-      matched.each do |combination|
-        count = 0
-        count +=1 if(user_agent == combination.user_agent && !interogate_params[:user_agent].blank?)  
-        count +=1 if(dhcp_fingerprint == combination.dhcp_fingerprint && !interogate_params[:dhcp_fingerprint].blank?)
-        count +=1 if(dhcp_vendor == combination.dhcp_vendor && !interogate_params[:dhcp_vendor].blank?)
-        count +=1 if(mac_vendor == combination.mac_vendor && !interogate_params[:mac].blank? && mac_vendor)
-        if(count >= top_matched && count > 0)
-          top = combination 
-          top_matched = count
-        end
-      end
-      @combination = top
-    end
+    @combination = Combination.where(:user_agent =>user_agent, :dhcp_fingerprint => dhcp_fingerprint, :dhcp_vendor_id => dhcp_vendor, :mac_vendor => mac_vendor).first
 
     respond_to do |format|
-      unless @combination.nil?
-        format.json { render 'combinations/show', status: :found, location: @combination }
-        Thread.new do
-          Combination.create(:user_agent => user_agent, :dhcp_fingerprint => dhcp_fingerprint, :dhcp_vendor => dhcp_vendor, :mac_vendor => mac_vendor, :device => @combination.device)  
-        end
-      else
-        if params[:wait]
-          @combination = Combination.create(:user_agent => user_agent, :dhcp_fingerprint => dhcp_fingerprint, :dhcp_vendor => dhcp_vendor, :mac_vendor => mac_vendor)
-          @combination.process
-          format.json { render 'combinations/show', status: :found, location: @combination }
-        else
-          format.json { render json: {:message => 'Not found. Will process. Try again in a few moments'}, :status => :not_found }
-          Thread.new do
-            combination = Combination.create(:user_agent => user_agent, :dhcp_fingerprint => dhcp_fingerprint, :dhcp_vendor => dhcp_vendor, :mac_vendor => mac_vendor)
-            combination.process
-          end
-        end
+      if @combination.nil?
+        @combination = Combination.create!(:user_agent => user_agent, :dhcp_fingerprint => dhcp_fingerprint, :dhcp_vendor => dhcp_vendor, :mac_vendor => mac_vendor, :submitter => @current_user)
+        @combination.process
       end
+      format.json { render 'combinations/show', status: :found, location: @combination }
     end
   end
 
