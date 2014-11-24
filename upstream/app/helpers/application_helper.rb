00934f978de5787351ee14917f9870deaad69358
Julien Semaan <jul.semaan@gmail.com>
2014-11-24 08:47:55 -0500

    Added stats

--- upstream/app/helpers/application_helper.rb
+++ upstream/app/helpers/application_helper.rb
@@ -17,4 +17,23 @@ module ApplicationHelper
   def current_user_admin?
     @current_user && @current_user.admin?
   end
+
+  def new_combinations(from_when = 1.day.ago)
+    Combination.where('created_at > ?', from_when)
+  end
+
+  def new_user_agents(from_when = 1.day.ago)
+    UserAgent.where('created_at > ?', from_when)
+  end
+
+  def devices_discovered(from_when = 20.year.ago)
+    devices = []
+    Device.all.each do |d|
+      if !d.combinations.empty? && !devices.include?(d) && !d.combinations.where('created_at > ?', from_when).empty?
+        devices << d
+      end
+    end
+    devices
+  end
+
 end
--- upstream/lib/tasks/fbcache.rake
+++ upstream/lib/tasks/fbcache.rake
@@ -10,4 +10,14 @@ namespace :fbcache do
     Combination.first.find_matching_discoverers
   end
 
+  task refresh_stats: :environment do
+    puts ActionController::Base.new.expire_fragment 'stats'
+    require 'net/http'
+    http = Net::HTTP.new("fingerbank.inverse.ca", 443)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    resp = http.get("/stats")
+    puts "Done" 
+  end
+
 end
