2e551e74e56e4b04a633f0a3f992525b55453a24
Julien Semaan <jul.semaan@gmail.com>
2014-12-08 13:39:15 -0500

    Added command line environment

---
+++ upstream/config/environments/command_line.rb
@@ -0,0 +1,26 @@
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # Code is not reloaded between requests.
+  config.cache_classes = true
+
+  # add bootstrap assets to be compiled
+  config.assets.paths << Rails.root.join("app", "assets", "bootstrap")
+
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both threaded web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = true
+
+  config.action_controller.perform_caching = true
+
+  # Enable Rack::Cache to put a simple HTTP cache in front of your application
+  # Add `rack-cache` to your Gemfile before enabling this.
+  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
+  # config.action_dispatch.rack_cache = true
+
+  # Set to :debug to see everything in the log.
+  config.log_level = :debug
+
+end
