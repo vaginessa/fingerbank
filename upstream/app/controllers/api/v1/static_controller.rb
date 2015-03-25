986d114a09c5befbda34267f3d78c8ed1b42746d
Julien Semaan <jul.semaan@gmail.com>
2015-03-25 11:37:20 -0400

    Add anonymous download of the db

--- upstream/app/controllers/api/v1/static_controller.rb
+++ upstream/app/controllers/api/v1/static_controller.rb
@@ -1,4 +1,20 @@
 class Api::V1::StaticController < Api::ApiController
+
+  def validate_key
+    if !ENV['PACKETFENCE_KEY'].nil? && params[:key] == ENV['PACKETFENCE_KEY']
+      record_anonymous_user
+      return
+    end 
+    super
+  end
+
+  def record_anonymous_user
+      date = Time.now
+      open(ENV['ANONYMOUS_STATS_FILE'], 'a') do |f|
+        f.puts "#{date},#{params[:key]},#{controller_name}/#{action_name},#{request.remote_addr}"
+      end 
+  end
+  
   def download
     db_fname = Rails.root.join('db', 'package', "packaged.sqlite3")
     send_file(db_fname, :filename => "packaged.sqlite3", :type => "application/x-sqlite3")
