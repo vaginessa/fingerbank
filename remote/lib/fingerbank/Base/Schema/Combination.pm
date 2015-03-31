255e53450177aa96dfc37e18497f90244016328c
Derek Wuelfrath <dwuelfrath@inverse.ca>
2015-03-31 14:57:37 -0400

    Adjusted combination schema

--- remote/lib/fingerbank/Base/Schema/Combination.pm
+++ remote/lib/fingerbank/Base/Schema/Combination.pm
@@ -1,5 +1,7 @@
 package fingerbank::Base::Schema::Combination;
 
+use fingerbank::Model::Device;
+
 use base qw/DBIx::Class::Core/;
 
 __PACKAGE__->table('combination');
@@ -22,7 +24,9 @@ __PACKAGE__->set_primary_key('id');
 # See L<fingerbank::Base::CRUD::read>
 sub value {
     my ( $self ) = @_;
-    return $self->device_id;
+    my $value = fingerbank::Model::Device->read($self->device_id);
+    return $value->{'name'};
+#    return $self->device_id;
 }
 
 
@@ -42,9 +46,9 @@ __PACKAGE__->result_source_instance->view_definition("
     SELECT * FROM combination
     WHERE dhcp_fingerprint_id = ? OR dhcp_vendor_id = ? OR user_agent_id = ? OR (mac_vendor_id = ? OR mac_vendor_id IS NULL)
     ORDER BY
-    case when dhcp_fingerprint_id = ? then 1 else 0 END +
-    case when dhcp_vendor_id = ? then 1 else 0 END +
-    case when user_agent_id = ? then 1 else 0 END +
+    case when (dhcp_fingerprint_id = ? AND dhcp_fingerprint_id != '538') then 2 else 0 END +
+    case when (dhcp_vendor_id = ? AND dhcp_vendor_id != '4945') then 2 else 0 END +
+    case when (user_agent_id = ? AND user_agent_id != '6') then 2 else 0 END +
     case when (mac_vendor_id = ? OR (mac_vendor_id IS NULL AND ? IS NULL)) then 1 else 0 END
     DESC,
     score DESC LIMIT 1
