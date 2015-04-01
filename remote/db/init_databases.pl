7f308fc4d45c2d1017768a3b0438ac1ffae97582
Derek Wuelfrath <dwuelfrath@inverse.ca>
2015-04-01 10:48:09 -0400

    Improved databas initialization

--- remote/db/init_databases.pl
+++ remote/db/init_databases.pl
@@ -3,15 +3,38 @@
 use strict;
 use warnings;
 
+use Getopt::Long;
+use Pod::Usage;
+use Switch;
+
 BEGIN {
-    use lib "/usr/local/fingerbank/lib";
+    use lib "../lib";
     use fingerbank::Log;
     fingerbank::Log::init_logger;
 }
 
 use fingerbank::DB;
 
-fingerbank::DB::initialize_local;
-fingerbank::DB::fetch_upstream;
+my $database = "local";
+GetOptions ( "database=s" => \$database, );
+$database = lc($database);
+if ( !($database =~ /^local|upstream|both$/) ) {
+    pod2usage("\nthe 'database' argument must be 'local', 'upstream' or 'both'\n");
+}
+
+switch ( $database ) {
+    case 'local' {
+        fingerbank::DB::initialize_local;
+    }
+
+    case 'upstream' {
+        fingerbank::DB::fetch_upstream;
+    }
+
+    case 'both' {
+        fingerbank::DB::initialize_local;
+        fingerbank::DB::fetch_upstream;
+    }
+}
 
 1;
