ce4544b9f87aa8b0f2f0989dfa9659416f950044
Julien Semaan <jul.semaan@gmail.com>
2015-04-24 14:57:03 -0400

    add pagination to events

--- upstream/app/controllers/events_controller.rb
+++ upstream/app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all.limit(50).order('created_at DESC')
+    @events = Event.all.limit(50).order('created_at DESC').paginate(:page => params[:page], :per_page => 10)
 
     @events.each do |event|
       event.value = event.value.gsub(/\n/, '<br>')    
