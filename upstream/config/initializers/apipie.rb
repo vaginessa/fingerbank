7b3488b78444303824dcf4c808a498fa820bc345
Julien Semaan <jul.semaan@gmail.com>
2015-06-16 09:47:42 -0400

    don't validate API queries through apipie

--- upstream/config/initializers/apipie.rb
+++ upstream/config/initializers/apipie.rb
@@ -2,6 +2,7 @@ Apipie.configure do |config|
   config.app_name                = "Fingerbank"
   config.api_base_url            = "/api/v1"
   config.doc_base_url            = "/api_doc"
+  config.validate                = false
   config.default_version         = "1"
   config.app_info                = "All requests to the fingerbank API need to contain your API key in the URL parameters. You can find your key and regenerate it by accessing your account."
   # where is your API defined?
