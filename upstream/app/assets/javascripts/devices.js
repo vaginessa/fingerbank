99a6952cb3a9062489fcbb5ae69c6a2a68b5e832
Julien Semaan <jul.semaan@gmail.com>
2015-06-19 13:44:54 -0400

    rework devices expand button

--- upstream/app/assets/javascripts/devices.js
+++ upstream/app/assets/javascripts/devices.js
@@ -1,6 +1,12 @@
 function toggle_device(id){
   $('*[data-maximized-by="'+id+'"]').toggle()
   var elem = $('#toggle-childs-'+id)
-  if( elem.html() == "-" ) elem.html('+')
-  else if( elem.html() == "+" ) elem.html('-')
+  if( elem.hasClass('device-toggler-expand')) {
+    elem.removeClass('device-toggler-expand');
+    elem.addClass('device-toggler-shrink')
+  }
+  else if( elem.hasClass('device-toggler-shrink')) {
+    elem.removeClass('device-toggler-shrink');
+    elem.addClass('device-toggler-expand')
+  }
 }
