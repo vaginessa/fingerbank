c5aaf9a102a8e3db585a046123ef72617e2b2975
Julien Semaan <jul.semaan@gmail.com>
2015-09-11 09:35:48 -0400

    touchups to test script

--- upstream/test/test_api.sh
+++ upstream/test/test_api.sh
@@ -1,3 +1,7 @@
-time curl -X GET -d "{\"user_agent\":\"iPhone 7_1_2\"}" --header "Content-type: application/json" https://fingerbank.inverse.ca/api/v1/combinations/interrogate?key=$1
+# UserAgent.where("value like ?", "fingerbank_testing%").each do |u| u.combinations.each do |c| c.delete end end
+
+le_date=$(date)
+
+time curl -X GET -d "{\"user_agent\":\"fingerbank_testing iPhone 7_1_2 $le_date\"}" --header "Content-type: application/json" "https://fingerbank.inverse.ca/api/v1/combinations/interrogate?key=$1"
 
 
