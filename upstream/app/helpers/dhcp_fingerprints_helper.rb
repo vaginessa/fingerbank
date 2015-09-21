af01318b5deb25d0921defcb35ae6f9232013fe4
Julien Semaan <jul.semaan@gmail.com>
2015-09-21 13:29:05 -0400

    ameliorate dhcp fingerprint view

--- upstream/app/helpers/dhcp_fingerprints_helper.rb
+++ upstream/app/helpers/dhcp_fingerprints_helper.rb
@@ -1,2 +1,6 @@
 module DhcpFingerprintsHelper
+  def dhcp_fingerprint_search(fingerprint,scope = nil)
+    uri = scope == "unknown" ? "/combinations/unknown" : "/combinations"
+    "#{uri}?search=%5E#{URI.escape fingerprint}%24&fields%5B%5D=dhcp_fingerprints.value"
+  end
 end
