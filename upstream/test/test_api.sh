595efbfde81c5772e563ddc28b430677658ad918
Julien Semaan <jul.semaan@gmail.com>
2015-09-29 08:50:34 -0400

    fix bad env comparison

--- upstream/test/test_api.sh
+++ upstream/test/test_api.sh
@@ -1,7 +1,7 @@
 # UserAgent.where("value like ?", "fingerbank_testing%").each do |u| u.combinations.each do |c| c.delete end end
 
 le_date=$(date)
-if [ $RAILS_ENV="development" ]; then 
+if [ "$RAILS_ENV" == "development" ]; then 
   le_host="http://127.0.0.1:3000"
 else
   le_host="https://fingerbank.inverse.ca"
