5213c18f180146a61d7e05b6f68462d66da91f02
Julien Semaan <jul.semaan@gmail.com>
2015-09-30 12:56:55 -0400

    started integration with p0f

--- remote/perl/lib/fingerbank/Discoverers/TCPFingerprinting.pm
+++ remote/perl/lib/fingerbank/Discoverers/TCPFingerprinting.pm
@@ -2,54 +2,80 @@ package fingerbank::Discoverers::TCPFingerprinting;
 
 use Moose;
 
+use fingerbank::Constant qw($TRUE);
+use fingerbank::Status;
+use fingerbank::Util qw(is_error is_success);
+
+use IO::Socket::UNIX;
+
 sub match {
-    my ($args, $other_results) = @_;
+    my ($self, $args, $other_results) = @_;
     my $logger = fingerbank::Log::get_logger;
-    $logger->info("Change me. I'm not doing anything.");
-    return ($fingerbank::Status::OK, {
-        "created_at" => "2015-09-29T13 =>15 =>52.558Z", 
-        "device" => {
-            "created_at" => "2014-09-09T15 =>10 =>22.000Z", 
-            "id" => 264, 
-            "inherit" => 0, 
-            "mobile" => 1, 
-            "name" => "Apple iPhone", 
-            "parent_id" => 193, 
-            "parents" => [
-                {
-                    "approved" => 1, 
-                    "created_at" => "2014-09-09T15 =>09 =>52.000Z", 
-                    "id" => 193, 
-                    "inherit" => 0, 
-                    "mobile" => 1, 
-                    "name" => "Apple iPod, iPhone or iPad", 
-                    "parent_id" => 11, 
-                    "submitter_id" => undef, 
-                    "tablet" => 0, 
-                    "updated_at" => "2015-02-04T15 =>53 =>52.000Z"
-                }, 
-                {
-                    "approved" => 0, 
-                    "created_at" => "2014-09-09T15 =>09 =>50.000Z", 
-                    "id" => 11, 
-                    "inherit" => 0, 
-                    "mobile" => 0, 
-                    "name" => "Smartphones/PDAs/Tablets", 
-                    "parent_id" => undef, 
-                    "submitter_id" => undef, 
-                    "tablet" => 0, 
-                    "updated_at" => "2014-11-14T19 =>02 =>32.000Z"
-                }
-            ], 
-            "updated_at" => "2015-02-06T15 =>53 =>26.000Z"
-        }, 
-        "id" => 478340, 
-        "score" => 5, 
-        "updated_at" => "2015-09-29T13 =>15 =>52.697Z", 
-        "version" => undef
-    });
+    $logger->info("Trying to interrogate p0f with IP $args->{ip}.");
  
-    return $fingerbank::Status::NOT_FOUND; 
+    my @parts = split '\.', $args->{ip};
+
+    my $payload = pack("I C CCCC", 1345340929, 4, @parts);
+    # We pad zeros at the end as the IP can extend up to 16 (for IPv6)
+    foreach my $i (1..12){
+        $payload .= pack("n", 0);
+        $i++;
+    }
+
+    my $socket = IO::Socket::UNIX->new(
+        Type => SOCK_STREAM,
+        Peer => '/var/run/p0f.sock',
+    );
+
+    die "Can't create socket: $!" unless $socket;
+
+    $socket->send($payload);
+
+    my $response;
+    $socket->recv($response, 236);
+
+    use Data::Dumper;
+    my ($magic, $result, %info);
+
+    ($magic, $result, $response) = unpack("I I a*", $response);
+    ($info{first_seen}, $info{last_seen}, $info{total_conn}, $response) = unpack("I I I a*", $response); 
+    ($info{uptime_min}, $info{up_mod_days},$response) = unpack("I I a*", $response); 
+    ($info{last_nat}, $info{last_chg}, $response) = unpack("I I a*", $response); 
+    ($info{last_nat}, $info{bad_sw}, $info{os_match_q}, $response) = unpack("s C C a*", $response); 
+    ($info{os_name}, $info{os_flavor}, $response) = unpack("a32 a32 a*", $response); 
+    ($info{http_name}, $info{http_flavor}, $response) = unpack("a32 a32 a*", $response); 
+    ($info{link_type}, $info{language}, $response) = unpack("a32 a32 a*", $response); 
+
+    if($result eq 16){
+      $logger->debug("Success ! Found data through p0f.");
+      $self->_buildResult(\%info);
+    }
+    elsif($result eq 32){
+      $logger->debug("Unknown device to p0f.");
+      return $fingerbank::Status::NOT_FOUND;
+    }
+    elsif($result eq 0){
+      $logger->error("Invalid p0f query");
+      return $fingerbank::Status::INTERNAL_SERVER_ERROR;
+    }
+}
+
+sub _buildResult {
+    my ($self, $info) = @_;
+    my $result = {};
+
+
+    print Dumper($info);
+    # Get device info
+    my ( $status, $device ) = fingerbank::Model::Device->read($info->{os_name}, $TRUE);
+    use Data::Dumper;
+    print Dumper($device);
+    return $status if ( is_error($status) );
+
+
+    foreach my $key ( keys %$device ) {
+        $result->{device}->{$key} = $device->{$key};
+    }
 }
 
 1;
